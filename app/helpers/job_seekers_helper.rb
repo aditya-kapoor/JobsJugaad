629b47a2b2425ece6034db3e5d3ec553ea8d3994
aditya-kapoor <aditya.kapoor@vinsol.com>
2012-09-15 19:16:04 +0530

    Stylesheet Modifications

--- app/helpers/job_seekers_helper.rb
+++ app/helpers/job_seekers_helper.rb
@@ -1,2 +1,14 @@
 module JobSeekersHelper
+  
+  def get_errors(parameter)
+    str = "" 
+    unless @job_seeker.errors[parameter.to_sym].empty?
+      @job_seeker.errors[parameter.to_sym].each { |msg| str+=msg+", " }
+    end
+    str
+  end
+
+  def error_present?(parameter)
+    @job_seeker.errors[parameter.to_sym].empty?
+  end
 end
