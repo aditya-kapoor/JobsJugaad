561b717c9a494d2a5d772efb68d0ab8bf22246b2
aditya-kapoor <aditya.kapoor@vinsol.com>
2012-09-24 19:00:13 +0530

    Refactoring In Process

--- app/helpers/job_seekers_helper.rb
+++ app/helpers/job_seekers_helper.rb
@@ -7,4 +7,13 @@ module JobSeekersHelper
     end
     skill_set
   end
+
+  def get_status(seeker_id, job_id)
+    @job = JobApplication.find_by_job_seeker_id_and_job_id(seeker_id, job_id)
+    if @job.interview_on.nil?
+      "Applied"
+    else
+      "Interview On (#{@job.interview_on.strftime("%d-%m-%Y")})"
+    end
+  end
 end
