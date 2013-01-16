f1855f405824778ba1e66e660e0ff957cac6b435
aditya-kapoor <aditya.kapoor@vinsol.com>
2013-01-16 17:56:04 +0530

    Cap Changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,7 @@ namespace :gems do
     # run "echo '#{release_path}'"
     # run "cp #{current_path}/config/database.yml #{release_path}/config/"
     run "mv #{current_path}/config/database.yml.example #{current_path}/config/database.yml"
-    run "cd #{current_path} && RAILS_ENV=production bundle exec rake db:migrate"
+    run "cd #{current_path} && bundle exec rake db:migrate RAILS_ENV=production"
   end
 end
 
