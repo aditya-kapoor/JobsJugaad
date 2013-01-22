5f1d3f3014bd0b47e3f89f3777c9b9c8c14a25af
aditya-kapoor <aditya.kapoor@vinsol.com>
2013-01-22 18:13:01 +0530

    Cap Changes:

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,6 +1,25 @@
 $('document').ready(function(){
   $('#jobs-container').click(function(){
     $('table #all-jobs').slideToggle("slow")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
   })
   $('#employers-container').click(function(){
     $('table #all-employers').slideToggle("slow")
